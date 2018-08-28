34136f6ffa0b7bcffaccad48e3f125b545d1c966
lubnaqadah <lubna.alqadah@gmail.com>
2018-08-27 21:09:06 -0700

    updated package.jason

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div>
-<!--          <Route exact path = "/" component = {ProjectSkywritingDemo} />-->
           <Route exact path = "/" component = {Speakersgram} />
         </div>
       </BrowserRouter>
