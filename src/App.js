488a863303e9a1b6edd1a83ff930d4f63b0befe0
lubnaqadah <lubna.alqadah@gmail.com>
2018-08-27 21:25:29 -0700

    added an index file

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ProjectSkywritingDemo from './ProjectSkywritingDemo';
+//react nav stuff
+import { BrowserRouter, Route} from 'react-router-dom'
+import { createBrowserHistory } from 'history';
+import Speakersgram from './Components/Speakersgram/Speakersgram';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path = "/" component = {ProjectSkywritingDemo} />
+          <Route exact path = "/Speakersgram" component = {Speakersgram} />
+        </div>
+      </BrowserRouter>
+
+
+
+
+    );
+  }
+}
+
+export default App;
+/*
+<div className="App">
+  <ProjectSkywritingDemo urlParams={this.props.match.params} urlQueryString={this.props.location.search} />
+</div>
+*/
